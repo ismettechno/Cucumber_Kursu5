06099e8f1d2b2a870059134352e182b8e67c9cb8
TechnoStudy <ismettechno@gmail.com>
2023-03-23 16:48:01 +0000

    Cucumber cucumber kare disabled

--- src/test/java/StepDefinitions/_02_CountrySteps.java
+++ src/test/java/StepDefinitions/_02_CountrySteps.java
@@ -20,8 +20,8 @@ public class _02_CountrySteps {
     @When("create a country")
     public void createACountry() {
         dc.clickFunction(dc.addButton);
-        dc.sendKeysFunction(dc.nameInput, "isUlk11");
-        dc.sendKeysFunction(dc.codeInput, "11111");
+        dc.sendKeysFunction(dc.nameInput, "isUlk112");
+        dc.sendKeysFunction(dc.codeInput, "111112");
         dc.clickFunction(dc.saveButton); // TODO
     }
 
