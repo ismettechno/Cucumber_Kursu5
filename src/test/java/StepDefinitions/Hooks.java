a3ab66820aba00779fe61ab0a301a7ea5721b678
TechnoStudy <ismettechno@gmail.com>
2023-03-29 19:31:03 +0100

    Cucumber Runner Class ExtendReport screenshot

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -2,13 +2,25 @@ package StepDefinitions;
 
 import Utilities.GWD;
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
 
 public class Hooks {
 
 
     @After  // her senaryodan sonra çalışacak bölüm
-    public void after(){
+    public void after(Scenario senaryo){
         System.out.println("Senaryo bitti");
+
+        if (senaryo.isFailed()){
+            TakesScreenshot ts=(TakesScreenshot) GWD.getDriver();
+            byte[] hafizadakiHali=ts.getScreenshotAs(OutputType.BYTES);
+            senaryo.attach(hafizadakiHali, "img/png","screen shot");
+        }
+
         GWD.quitDriver();
     }
 
