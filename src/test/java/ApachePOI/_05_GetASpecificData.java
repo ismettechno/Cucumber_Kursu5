34b0e7f4fabab04c3c2af3c30e114e2ff2c59207
TechnoStudy <ismettechno@gmail.com>
2023-04-03 19:06:46 +0100

    ApachePOI - excel _05_GetASpecificData Cevap

--- src/test/java/ApachePOI/_05_GetASpecificData.java
+++ src/test/java/ApachePOI/_05_GetASpecificData.java
@@ -1,5 +1,10 @@
 package ApachePOI;
 
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
 /**
  * Kullanıcı aşağıdaki excelde, 1.sutundaki bilgilerden istediğini girecek
  * karşılığında bu bilginin satırındaki karşılık gelen bilgilerin tamamı yazdırılacak.
@@ -9,11 +14,41 @@ package ApachePOI;
  */
 public class _05_GetASpecificData {
     public static void main(String[] args) {
+        System.out.print("Aranacak kelimeyi giriniz=");
+        Scanner oku=new Scanner(System.in);
+        String arananKelime=oku.next();
 
-
-
+        String donenSonuc=exceldenBul(arananKelime);
+        System.out.println("donenSonuc = " + donenSonuc);
     }
 
+    public static String exceldenBul(String aranan){
+        String donecek="";
+        //excelden okuma işlemleri
+        String path="src/test/java/ApachePOI/resource/LoginData.xlsx";
+
+        Sheet sheet=null;
+        try {
+            FileInputStream inputStream = new FileInputStream(path);
+            Workbook workbook = WorkbookFactory.create(inputStream);
+            sheet = workbook.getSheetAt(0);
+        }
+        catch (Exception e){
+        }
+
+        for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+
+            if (sheet.getRow(i).getCell(0).toString().equalsIgnoreCase(aranan)){
+
+                for (int j = 1; j < sheet.getRow(i).getPhysicalNumberOfCells(); j++) {
+                   donecek+= sheet.getRow(i).getCell(j)+" ";
+                }
+
+            }
+        }
+
+        return donecek;
+    }
 
 
 }
