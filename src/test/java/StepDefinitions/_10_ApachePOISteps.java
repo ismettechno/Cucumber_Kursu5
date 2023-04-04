dec2fbdb0d5a5cea7826f02743048440ae43c614
TechnoStudy <ismettechno@gmail.com>
2023-04-04 18:27:21 +0100

    ApachePOI -cucumber scenario - steps -2

--- src/test/java/StepDefinitions/_10_ApachePOISteps.java
+++ src/test/java/StepDefinitions/_10_ApachePOISteps.java
@@ -8,28 +8,30 @@ import io.cucumber.java.en.When;
 import java.util.ArrayList;
 
 public class _10_ApachePOISteps {
-    DialogContent dc=new DialogContent();
+    DialogContent dc = new DialogContent();
+
     @When("User Create citizenship with ApacehPOI")
     public void userCreateCitizenshipWithApacehPOI() {
-       ArrayList< ArrayList<String> > tablo=
-               ExcelUtility.getData("src/test/java/ApachePOI/resource/ApacheExcel2.xlsx","testCitizen",2);
+        ArrayList<ArrayList<String>> tablo =
+                ExcelUtility.getData("src/test/java/ApachePOI/resource/ApacheExcel2.xlsx", "testCitizen", 2);
 
-       for(ArrayList<String> satir: tablo){
-           dc.clickFunction(dc.addButton);
-           dc.sendKeysFunction(dc.nameInput,satir.get(0));
-           dc.sendKeysFunction(dc.shortName,satir.get(1));
-           dc.clickFunction(dc.saveButton);
-           dc.verifyContainsTextFunction(dc.successMessage,"success");
-       }
+        for (ArrayList<String> satir : tablo) {
+            dc.clickFunction(dc.addButton);
+            dc.sendKeysFunction(dc.nameInput, satir.get(0));
+            dc.sendKeysFunction(dc.shortName, satir.get(1));
+            dc.clickFunction(dc.saveButton);
+            dc.verifyContainsTextFunction(dc.successMessage, "success");
+        }
     }
 
     @Then("User Delete citizenship with ApacehPOI")
     public void userDeleteCitizenshipWithApacehPOI() {
-        ArrayList< ArrayList<String> > tablo=
-                ExcelUtility.getData("src/test/java/ApachePOI/resource/ApacheExcel2.xlsx","testCitizen",1);
+        ArrayList<ArrayList<String>> tablo =
+                ExcelUtility.getData("src/test/java/ApachePOI/resource/ApacheExcel2.xlsx", "testCitizen", 1);
 
-        for(ArrayList<String> satir: tablo){
-             dc.deleteItem(satir.get(0));
+        for (ArrayList<String> satir : tablo) {
+            dc.deleteItem(satir.get(0));
+            dc.verifyContainsTextFunction(dc.successMessage, "success");
         }
     }
 }
