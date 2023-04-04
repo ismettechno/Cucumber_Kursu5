581e6a37a8137a64138b55a4f7e6afb67d44d386
TechnoStudy <ismettechno@gmail.com>
2023-04-04 18:04:39 +0100

    ApachePOI - excel excel to ArrayList metod - tam parameter

--- src/test/java/ApachePOI/_09_Tasks.java
+++ src/test/java/ApachePOI/_09_Tasks.java
@@ -14,33 +14,34 @@ import java.util.Scanner;
  */
 public class _09_Tasks {
     public static void main(String[] args) {
+        String path="src/test/java/ApachePOI/resource/ApacheExcel2.xlsx";
+        String sheetName ="testCitizen";
+
         System.out.print("İstediğiniz sütun sayısı=");
         Scanner oku=new Scanner(System.in);
         int columnCnt=oku.nextInt();
 
-        ArrayList< ArrayList<String> > donenSonuc=getData(columnCnt);  // String[][]
+        ArrayList< ArrayList<String> > donenSonuc=getData(path, sheetName, columnCnt);  // String[][]
         System.out.println("donenSonuc = " + donenSonuc);
     }
 
 
-    public static ArrayList< ArrayList<String> > getData(int colCnt){
+    public static ArrayList< ArrayList<String> > getData(String path, String sheetName, int colCnt){
         ArrayList< ArrayList<String> > tablo=new ArrayList<>();
-
-        String path="src/test/java/ApachePOI/resource/ApacheExcel2.xlsx";
 
         Sheet sheet =null;
         try {
             FileInputStream inputStream = new FileInputStream(path);
             Workbook workbook = WorkbookFactory.create(inputStream);
-            sheet = workbook.getSheetAt(1);
+            sheet = workbook.getSheet(sheetName); // getSheet(testCitizen)  getSheetAt(0,1,2,3)
         }
         catch (Exception e){
             System.out.println("e.getMessage() = " + e.getMessage());
         }
 
-        for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+        for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) { // zoo.length
             ArrayList<String> satir=new ArrayList<>();
-            for (int j = 0; j < colCnt; j++) {
+            for (int j = 0; j < colCnt; j++) {   // zoo[i].length
                   satir.add(sheet.getRow(i).getCell(j).toString());
             }
 
