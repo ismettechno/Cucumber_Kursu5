502346804d0925c721e0d370425557716b4df8b2
TechnoStudy <ismettechno@gmail.com>
2023-04-17 18:51:43 +0100

    JDBC - JDBC DBList

--- src/test/java/StepDefinitions/_11_JDBCSteps.java
+++ src/test/java/StepDefinitions/_11_JDBCSteps.java
@@ -1,10 +1,21 @@
 package StepDefinitions;
 
+import Utilities.DBUtility;
 import io.cucumber.java.en.Then;
+
+import java.util.ArrayList;
 
 public class _11_JDBCSteps {
     @Then("Send The query the database {string} and control match")
     public void sendTheQueryTheDatabaseAndControlMatch(String sorgu) {
+        // DB den gerekli bilgileri alalım
+        ArrayList< ArrayList<String> > dbList= DBUtility.getListData(sorgu);
+
+        for (int i = 0; i < dbList.size(); i++) {
+            System.out.println(dbList.get(i).get(1).trim());
+        }
+
+
 
     }
 }
