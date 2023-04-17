022d414a092a443bad93f70c0bc70d8f34b08130
TechnoStudy <ismettechno@gmail.com>
2023-04-17 18:05:34 +0100

    JDBC - getListData

--- src/test/java/_JDBC/Gun2/_01_GetAllRowColumn.java
+++ src/test/java/_JDBC/Gun2/_01_GetAllRowColumn.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class _01_GetAllRowColumn extends JDBCParent {
 
@@ -20,8 +21,8 @@ public class _01_GetAllRowColumn extends JDBCParent {
         System.out.println("columnCount = " + columnCount);
 
         for (int i = 1; i <= columnCount; i++) {
-            System.out.print("Column name="+ rsmd.getColumnName(i));
-            System.out.println("\tColumn type="+ rsmd.getColumnTypeName(i));
+            System.out.print("Column name=" + rsmd.getColumnName(i));
+            System.out.println("\tColumn type=" + rsmd.getColumnTypeName(i));
         }
 
     }
@@ -31,18 +32,17 @@ public class _01_GetAllRowColumn extends JDBCParent {
         // language tablosundaki tüm satırları ve tüm sütunları yazdırınız,
         // aynı mysql sonuç ekranında olduğu gibi
 
-        ResultSet rs = statement.executeQuery("select * from language");
-        ResultSetMetaData rsmd = rs.getMetaData();
+        ResultSet rs = statement.executeQuery("select * from language"); // data + metdata
+        ResultSetMetaData rsmd = rs.getMetaData(); // data dışındaki bilgileri: kolon sayısı,ismi,tipleri
 
         for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-            System.out.print(rsmd.getColumnName(i)+"\t");
+            System.out.print(rsmd.getColumnName(i) + "\t");
         }
         System.out.println();
 
-        while (rs.next())
-        {
+        while (rs.next()) {
             for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-                System.out.print(rs.getString(i)+"\t");
+                System.out.print(rs.getString(i) + "\t");
             }
 
             System.out.println();
@@ -51,4 +51,48 @@ public class _01_GetAllRowColumn extends JDBCParent {
     }
 
 
+    @Test
+    public void test3() throws SQLException {
+        // bir metoda sorguyu("select * from language") gönderiniz,
+        // metod size sorgunun datasını bir ArrayList olarak döndürsün
+
+        String sql = "select * from city";
+        ArrayList<ArrayList<String>> donenSonuc = getListData(sql);
+        System.out.println("donenSonuc = " + donenSonuc);
+    }
+
+    public ArrayList<ArrayList<String>> getListData(String sql) {
+        ArrayList<ArrayList<String>> tablo = new ArrayList<>();
+
+        try {
+            ResultSet rs = statement.executeQuery(sql);
+
+            while (rs.next()) {
+
+                ArrayList<String> satir = new ArrayList<>();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    satir.add(rs.getString(i));
+                }
+
+                tablo.add(satir);
+            }
+        }
+        catch (Exception e){
+            System.out.println("e.getMessage() = " + e.getMessage());
+        }
+
+        return tablo;
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
