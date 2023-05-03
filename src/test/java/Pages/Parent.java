16d1cc75c6bf795e6136f85e576f97f849067435
TechnoStudy <ismettechno@gmail.com>
2023-05-03 10:24:43 +0100

    Jenkins ayarları (defaut build)) t3

--- src/test/java/Pages/Parent.java
+++ src/test/java/Pages/Parent.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 public class Parent {
 
     WebDriverWait wait=new WebDriverWait(GWD.getDriver(),
-            Duration.ofSeconds(5));
+            Duration.ofSeconds(15));
 
     public void sendKeysFunction(WebElement element, String yazi)
     {
